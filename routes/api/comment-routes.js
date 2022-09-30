4eb1e32ff4141fce80056bc2864bdb53845b8498
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-10-01 05:33:08 +0800

    index update fixes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../../utils/auth');
-const { Comment } = require('../../models/index-model');
+const { Comment } = require('../../models');
 
 
 // route to get all comments on post page 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models/index-model');
+const { User } = require('../../models');
 
 //create user
 router.post('/user', async (req, res) => {
